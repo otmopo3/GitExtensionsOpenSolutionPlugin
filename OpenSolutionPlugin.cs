b58de3f642d5a4a04c3b9a4c5987ccc938f65aff
Novikov <novikov@macroscop.com>
2018-10-05 20:19:45 +0500

    first comit.

---
+++ OpenSolutionPlugin.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using GitUIPluginInterfaces;
+using OpenSolution.Properties;
+using ResourceManager;
+
+namespace OpenSolution
+{
+	[Export(typeof(IGitPlugin))]
+	public sealed class OpenSolutionPlugin : GitPluginBase, IGitPluginForRepository
+	{
+		private readonly StringSetting _preferedSolutonsSetting = new StringSetting("Prefered files", "");
+
+		public OpenSolutionPlugin()
+		{
+			SetNameAndDescription("Open Default Solution");
+			Translate();
+			Icon = Resources.IconFindLargeFiles;
+		}
+
+		public override IEnumerable<ISetting> GetSettings()
+		{
+			yield return _preferedSolutonsSetting;
+		}
+
+		public override bool Execute(GitUIEventArgs args)
+		{
+			var workingDir = args.GitModule.WorkingDir;
+
+			string slnFilePath;
+			var foundSlnFile = TryFindSlnFile(workingDir, out slnFilePath);
+			if (foundSlnFile)
+				System.Diagnostics.Process.Start(slnFilePath);
+
+			return foundSlnFile;
+		}
+
+		private bool TryFindSlnFile(string dirPath, out string slnFilePath)
+		{
+			var files = Directory.GetFiles(dirPath, "*.sln");
+
+			slnFilePath = string.Empty;
+
+			string defaultFile = string.Empty;
+
+			var preferedSOlutions = _preferedSolutonsSetting.ValueOrDefault(Settings).Split(',', ';', '|');
+
+			foreach (var preferedSolution in preferedSOlutions)
+			{
+				var file = files.FirstOrDefault(f => f.Contains(preferedSolution));
+
+				defaultFile = file;
+
+				if (string.IsNullOrEmpty(file))
+					continue;
+
+				slnFilePath = file;
+
+				return true;
+			}
+
+			if (string.IsNullOrEmpty(defaultFile))
+				return false;
+
+			slnFilePath = defaultFile;
+
+			return true;
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GitExtensions")]
+[assembly: AssemblyDescription("GitExtensions is a graphical interface for Git")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GitExtensions")]
+[assembly: AssemblyProduct("GitExtensions")]
+[assembly: AssemblyCopyright("Copyright © 2008-2018 GitExt Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: System.CLSCompliant(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("68315e13-47a3-4124-8c19-47f80a7a1518")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.51.05")]
+[assembly: AssemblyFileVersion("2.51.05")]
+[assembly: AssemblyInformationalVersion("2.51.05")]
